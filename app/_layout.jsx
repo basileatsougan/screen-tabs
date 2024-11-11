a0b55cc8afab82ab4a9dc3e27cfb3e8fe62a8b9f
atsougan870@gmail.com <atsougan870@gmail.com>
2024-11-11 12:52:42 +0200

    create react tab bar initials

---
+++ app/_layout.jsx
@@ -0,0 +1,36 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+
+const Layout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default Layout;
